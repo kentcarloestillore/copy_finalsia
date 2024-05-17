f3b6e4c8a2e4b2bdf9f020ba26a238148d7f710e
kentcarloestillore <152847075+kentcarloestillore@users.noreply.github.com>
2024-05-17 16:23:00 +0800

    Update EmployeeController.php

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    public function pdf(){
+        $products = Product::orderby('name')->get();
+        $pdf = PDF::loadView('product-list', compact('products'));
+        return $pdf->download('product-list.pdf');
+    }
+
+    function generateCSV() {
+        $flights = Flight::orderBy('id')->get();
+
+        $filename = '../storage/flight.csv';
+
+        $file = fopen($filename,'w+');
+
+        foreach($flights as $f){
+            fputcsv($file,[
+                $f->flight_code,
+                $f->origin,
+                $f->destination,
+                $f->price,
+            ]);
+        }
+
+        fclose($file);
+
+        return response()->download($filename);
+    }
 }
